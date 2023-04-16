9cb7e9ec8ec3eb3fb5365b1b5dd7d9414ad0970d
hililyy <joun406@gmail.com>
2023-04-16 23:26:41 +0900

    [ADD] AutoLayout Conde Anchor

--- AutoLayoutPractice/ViewController.swift
+++ AutoLayoutPractice/ViewController.swift
@@ -11,9 +11,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let myButton = UIButton.init(type: .system)
+        myButton.setTitle("My Button", for: .normal)
+        
+        self.view.addSubview(myButton)
+        
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 100).isActive = true
+        myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
     }
-
-
 }
-
