35cded1809bc19aa4cdf2bfc4c8357dd00d313b2
강조은 <joun406@gmail.com>
2023-04-21 16:38:32 +0900

    [ADD] 테이블 뷰 셀 UI 구성

---
+++ ExpandCell/ExpandCell/ExpandCell.swift
@@ -0,0 +1,25 @@
+//
+//  ExpandCell.swift
+//  ExpandCell
+//
+//  Created by 강조은 on 2023/04/21.
+//
+
+import UIKit
+
+class ExpandCell: UITableViewCell {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ExpandCell/ExpandCell/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ExpandCell
+//
+//  Created by 강조은 on 2023/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "expandCell_ID", for: indexPath) as! ExpandCell
+        cell.descriptionLabel.text = "설명입니다."
+        return cell
+    }
+    
+    
+}
