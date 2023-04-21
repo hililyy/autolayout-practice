ddfd6dbaaf1d9a9fd248daeff5c61443db17e020
강조은 <joun406@gmail.com>
2023-04-21 17:14:50 +0900

    [FEAT] 테이블뷰 클릭에 따른 Label Expand 설정

--- ExpandCell/ExpandCell/ViewController.swift
+++ ExpandCell/ExpandCell/ViewController.swift
@@ -9,25 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var dataModels = [ExpandDataModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let textArray = ["shortText", "long long long long long long long long long long long long long long long long long long ", "shortText", "long long long long long long long long long long long long long long ", "shortText", "long long long long long long long long long long long long long long long long long long  "]
+        
+        for (_, value) in textArray.enumerated() {
+            dataModels.append(ExpandDataModel.init(description: value, isExpand: false))
+        }
     }
-
-
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return dataModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "expandCell_ID", for: indexPath) as! ExpandCell
-        cell.descriptionLabel.text = "설명입니다."
+        cell.descriptionLabel.text = dataModels[indexPath.row].description
+        
+        if dataModels[indexPath.row].isExpand == true {
+            cell.descriptionLabel.numberOfLines = 0
+        } else {
+            cell.descriptionLabel.numberOfLines = 1
+        }
+        
+        cell.selectionStyle = .none
+        
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        dataModels[indexPath.row].isExpand = !dataModels[indexPath.row].isExpand
+        
+        tableView.reloadRows(at: [indexPath], with: .none)
+    }
 }
+
+struct ExpandDataModel {
+    var description: String
+    var isExpand: Bool
+}
+
