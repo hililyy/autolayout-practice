caa087a9ff9e68442b1901187a8c624be0449a25
강조은 <joun406@gmail.com>
2023-04-21 16:16:16 +0900

    [FEAT] 키보드 버튼 이벤트 기능 추가

--- CustomKeyboard/CustomKeyboard/CustomKeyboard.swift
+++ CustomKeyboard/CustomKeyboard/CustomKeyboard.swift
@@ -7,6 +7,15 @@
 
 import UIKit
 
+protocol CustomKeyboardDelegate {
+    func keyboardTapped(str: String)
+}
+
 class CustomKeyboard: UIView {
     
+    var delegate: CustomKeyboardDelegate?
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        delegate?.keyboardTapped(str: sender.titleLabel!.text!)
+    }
 }
--- CustomKeyboard/CustomKeyboard/ViewController.swift
+++ CustomKeyboard/CustomKeyboard/ViewController.swift
@@ -7,18 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CustomKeyboardDelegate {
 
     @IBOutlet weak var firstTextField: UITextField!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let myKeyboard = Bundle.main.loadNibNamed("CustomKeyboard", owner: nil, options: nil)
         let myKeyboardView = myKeyboard?.first as! CustomKeyboard
-        
+        myKeyboardView.delegate = self
         firstTextField.inputView = myKeyboardView  // 시스템 키보드 대신 내가 원하는 뷰를 출력
+    }
+    
+    func keyboardTapped(str: String) {
+        print(str)
+        
+        let oldNumber = Int(firstTextField.text!)
+        var newNumber = Int(str)
+        
+        if str == "00" && oldNumber != nil {
+            newNumber = oldNumber! * 100
+        }
+        
+        if str == "000" && oldNumber != nil {
+            newNumber = oldNumber! * 1000
+        }
+        
+        if let hasNumber = newNumber {
+            let numberFormatter = NumberFormatter()
+            numberFormatter.numberStyle = .decimal
+            
+            let formatted = numberFormatter.string(from: NSNumber(value: hasNumber))
+            
+            firstTextField.text = String(describing: formatted)
+        }
     }
 }
 
