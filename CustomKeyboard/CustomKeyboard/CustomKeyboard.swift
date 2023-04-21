4d55859f04992196fbf107190cd4ba87c40224ad
강조은 <joun406@gmail.com>
2023-04-21 15:56:34 +0900

    [ADD] 커스텀 키보드 추가

---
+++ CustomKeyboard/CustomKeyboard/CustomKeyboard.swift
@@ -0,0 +1,12 @@
+//
+//  CustomKeyboard.swift
+//  CustomKeyboard
+//
+//  Created by 강조은 on 2023/04/21.
+//
+
+import UIKit
+
+class CustomKeyboard: UIView {
+    
+}
---
+++ CustomKeyboard/CustomKeyboard/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CustomKeyboard
+//
+//  Created by 강조은 on 2023/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myKeyboard = Bundle.main.loadNibNamed("CustomKeyboard", owner: nil, options: nil)
+        let myKeyboardView = myKeyboard?.first as! CustomKeyboard
+        
+        firstTextField.inputView = myKeyboardView  // 시스템 키보드 대신 내가 원하는 뷰를 출력
+    }
+}
+
+
+/**
+ 보안 프로그램에서 사용하는 키보드에서 사용하는 개념
+ 스택뷰를 이용해 랜덤 버튼을 커스텀하기 쉬움
+ 커스텀뷰의 높이, 크기에 관계없이 기존 키보드 크기에 맞추어 나타남
+ */
