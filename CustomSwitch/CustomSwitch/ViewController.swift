e3df4f94fd1585bc367924211342fc3fb5c44c7e
hililyy <joun406@gmail.com>
2023-04-23 14:58:01 +0900

    [ADD] Custom Switch UI 구성

---
+++ CustomSwitch/CustomSwitch/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CustomSwitch
+//
+//  Created by 강조은 on 2023/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var switchBtn: UIButton!
+    @IBOutlet weak var switchBG: UIView!
+    @IBOutlet weak var btnCenterX: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func touchBtn(_ sender: UIButton) {
+        
+    }
+}
