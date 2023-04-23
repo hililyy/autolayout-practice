bc4226d72b5f9e7c59ba2b90fcc6281168e88f5c
hililyy <joun406@gmail.com>
2023-04-23 16:49:41 +0900

    [FEAT] 로그인 이메일 정규표현식 기능 추가

--- SignIn/SignIn/ViewController.swift
+++ SignIn/SignIn/ViewController.swift
@@ -9,7 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var emailError: UILabel!
+    @IBOutlet weak var passwordError: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emailTextField.addTarget(self, action: #selector(textFieldEdited), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textFieldEdited), for: .editingChanged)  // UITextfield의 값이 변경될 때 호출
+    }
+    
+    @objc func textFieldEdited(textField: UITextField) {
+        if textField == emailTextField {
+            print("email text field \(String(describing: textField.text))")
+            
+            if isVaildEmail(email: textField.text) {
+                emailError.heightAnchor.constraint(equalToConstant: 0).isActive = true
+            } else {
+                
+            }
+        } else if textField == passwordTextField {
+            print("password text field \(String(describing: textField.text))")
+        }
+    }
+    
+    func isVaildEmail(email: String?) -> Bool {
+        guard let email = email else { return false }
+        let regEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let pred = NSPredicate(format: "SELF MATCHES %@", regEx)
+        return pred.evaluate(with: email)
     }
 }
