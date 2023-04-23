ac168a3b682aea7fe2297ec08bb9a58da28b55d6
hililyy <joun406@gmail.com>
2023-04-23 18:02:30 +0900

    [ADD] 채팅 셀 UI 구성

---
+++ Chat/Chat/MyCell.swift
@@ -0,0 +1,22 @@
+//
+//  MyCell.swift
+//  Chat
+//
+//  Created by 강조은 on 2023/04/23.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
