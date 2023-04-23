fbc670db27c9060126ea245dd086105654fe6ef1
hililyy <joun406@gmail.com>
2023-04-23 18:24:27 +0900

    [FEAT] 키보드 present, dismiss에 따른 텍스트뷰 위치 설정

--- Chat/Chat/ViewController.swift
+++ Chat/Chat/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chatTableView: UITableView!
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var inputViewBottomMargin: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 사용하려는 셀 등록
+        chatTableView.register(UINib(nibName: "MyCell", bundle: nil), forCellReuseIdentifier: "MyCell")
+        chatTableView.register(UINib(nibName: "YourCell", bundle: nil), forCellReuseIdentifier: "YourCell")
+        
+        // 키보드 관련 옵저버 - 상태를 알려주는 것
+        // 키보드 올라올 때
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        // 키보드 내려갈 때
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+    
+    @IBAction func sendString(_ sender: UIButton) {
+        
+    }
+    
+    @objc func keyboardWillShow(noti: Notification) {
+        let notiInfo = noti.userInfo!
+        let keyboardFrame = notiInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        let height = keyboardFrame.size.height - self.view.safeAreaInsets.bottom  // 키보드 나타났을 때 키보드와 텍스트뷰가 딱 달라붙도록
 
-
+        let animationDuration = notiInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.inputViewBottomMargin.constant = height
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillHide(noti: Notification) {
+        let notiInfo = noti.userInfo!
+        let animationDuration = notiInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+        
+        UIView.animate(withDuration: animationDuration) {
+            self.inputViewBottomMargin.constant = 0
+            self.view.layoutIfNeeded()
+        }
+    }
 }
 
