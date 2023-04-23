7d1f674743095958685311455dd696aea42e4694
hililyy <joun406@gmail.com>
2023-04-23 17:20:01 +0900

    [FEAT] 이메일, 패스워드 유효성 검사 애니메이션 추가

--- SignIn/SignIn/ViewController.swift
+++ SignIn/SignIn/ViewController.swift
@@ -11,29 +11,35 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var emailError: UILabel!
     @IBOutlet weak var passwordError: UILabel!
     
+    var emailErrorHeight: NSLayoutConstraint!
+    var pwErrorHeight: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         emailTextField.addTarget(self, action: #selector(textFieldEdited), for: .editingChanged)
         passwordTextField.addTarget(self, action: #selector(textFieldEdited), for: .editingChanged)  // UITextfield의 값이 변경될 때 호출
+        emailErrorHeight = emailError.heightAnchor.constraint(equalToConstant: 0)
+        pwErrorHeight = passwordError.heightAnchor.constraint(equalToConstant: 0)
     }
     
     @objc func textFieldEdited(textField: UITextField) {
-        if textField == emailTextField {
-            print("email text field \(String(describing: textField.text))")
+        UIView.animate(withDuration: 0.2) { [weak self] in
+            guard let self = self else { return }
             
-            if isVaildEmail(email: textField.text) {
-                emailError.heightAnchor.constraint(equalToConstant: 0).isActive = true
-            } else {
-                
+            switch textField {
+            case self.emailTextField:
+                self.emailErrorHeight.isActive = self.isVaildEmail(email: textField.text)
+            case self.passwordTextField:
+                self.pwErrorHeight.isActive = self.isVaildPW(pw: textField.text!)
+            default:
+                break
             }
-        } else if textField == passwordTextField {
-            print("password text field \(String(describing: textField.text))")
+            
+            self.view.layoutIfNeeded()
         }
     }
     
@@ -41,6 +47,13 @@ class ViewController: UIViewController {
         guard let email = email else { return false }
         let regEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let pred = NSPredicate(format: "SELF MATCHES %@", regEx)
+        
         return pred.evaluate(with: email)
+    }
+    
+    func isVaildPW(pw: String?) -> Bool {
+        guard let pw = pw else { return false }
+        
+        return !(pw.count < 8)
     }
 }
