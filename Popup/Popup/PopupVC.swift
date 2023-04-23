8a73407e77003175f81fdd1751734fd4af267a82
hililyy <joun406@gmail.com>
2023-04-23 15:51:44 +0900

    [ADD] Popup UI 구성

---
+++ Popup/Popup/PopupVC.swift
@@ -0,0 +1,15 @@
+//
+//  PopupVC.swift
+//  Popup
+//
+//  Created by 강조은 on 2023/04/23.
+//
+
+import UIKit
+
+class PopupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
